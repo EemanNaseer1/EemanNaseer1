bd79fecd0aa42f3318a8e35d94ea635536d0f59a
EemanNaseer1 <eemannaseer1@gmail.com>
2024-07-12 02:55:25 +0500

    project 3

--- lib/untitle_file.dart
+++ lib/untitle_file.dart
@@ -6,9 +6,77 @@ class UntitleFile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-    body: Row(children: [CircleAvatar(radius: 20,backgroundColor: Colors.purpleAccent,),CircleAvatar(radius: 20,backgroundColor: Colors.blue,)],)
-          ,
-          
+    body: Container(
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Row(mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+              Container(
+                child: Image.asset("assets/images/stylegirlframe.jpg"),
+              )
+            ],
+            ),
+            Row(mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+              Container(
+                child: Image.asset("assets/images/frame.jpg"),
+                padding: EdgeInsets.only(top: 30),
+              )
+            ],
+            ),
+            Row(mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+              Container(
+                child: Image.asset("assets/images/flat.jpg"),
+                padding: EdgeInsets.only(top: 20),
+              )
+            ],
+            ),
+            Row(mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+              Container(
+                child: Image.asset("assets/images/deal.jpg"),
+                padding: EdgeInsets.only(top: 20),
+              )
+            ],
+            ),
+            Row(mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+              Container(
+                child: Image.asset("assets/images/special.jpg"),
+                padding: EdgeInsets.only(top: 20),
+              )
+            ],
+            ),
+            Row(mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+              Container(
+                child: Image.asset("assets/images/heel.jpg"),
+                padding: EdgeInsets.only(top: 20),
+              )
+            ],
+            ),
+            Row(mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+              Container(
+                child: Image.asset("assets/images/trending.jpg"),
+                padding: EdgeInsets.only(top: 20),
+              )
+            ],
+            ),
+            Row(mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+              Container(
+                child: Image.asset("assets/images/sale.jpg"),
+                padding: EdgeInsets.only(top: 20),
+              )
+            ],
+            ),
+          ],
+        ),
+      ),
+    )
     );
   }
 }
